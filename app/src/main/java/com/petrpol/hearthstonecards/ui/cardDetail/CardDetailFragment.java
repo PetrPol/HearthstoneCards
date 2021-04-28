5e7ec5f61aaf675072d57a5e45d6f5ebd9808b49
Petr Polivka <polivkapetr96@gmail.com>
2021-04-28 20:03:07 +0200

    Back arrow on detail fragment fix

--- app/src/main/java/com/petrpol/hearthstonecards/ui/cardDetail/CardDetailFragment.java
+++ app/src/main/java/com/petrpol/hearthstonecards/ui/cardDetail/CardDetailFragment.java
@@ -2,6 +2,7 @@ package com.petrpol.hearthstonecards.ui.cardDetail;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -9,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.petrpol.hearthstonecards.R;
 import com.petrpol.hearthstonecards.databinding.FragmentCardDetailBinding;
@@ -16,15 +18,16 @@ import com.petrpol.hearthstonecards.databinding.FragmentCardDetailBinding;
 public class CardDetailFragment extends Fragment {
 
     private CardDetailViewModel cardDetailViewModel;
+    private View root;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-
         //Data binding create view
         FragmentCardDetailBinding mBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_card_detail, container, false);
-        View root = mBinding.getRoot();
+        root = mBinding.getRoot();
+        setHasOptionsMenu(true);
 
         //Get cardID from bundle
         String CardID = CardDetailFragmentArgs.fromBundle(getArguments()).getCardId();
@@ -35,5 +38,12 @@ public class CardDetailFragment extends Fragment {
         cardDetailViewModel.loadCard(CardID);
 
         return root;
+    }
+
+    /** Handles back arrow in Options menu - pops back stack */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Navigation.findNavController(root).popBackStack();
+        return false;
     }
 }
